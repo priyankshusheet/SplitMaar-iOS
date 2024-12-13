845168aa128e46540c6dc019deb8e079b21ce404
Priyankshu Sheet <135867558+priyankshusheet@users.noreply.github.com>
2024-12-13 21:51:10 +0530

    Add files via upload

---
+++ SplitMaar/SpiltMaar/SpiltMaar/SpiltMaarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiltMaarApp.swift
+//  SpiltMaar
+//
+//  Created by Priyankshu Sheet on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SpiltMaarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
